15f48f6f9836cf3e7bed0eef5a75db21e50e1dfe
nneuro <nneuro2013@gmail.com>
2019-02-26 21:53:23 +0300

    Не работает task3, но работает train. Не могу понять

---
+++ task3_for.py
@@ -0,0 +1,10 @@
+score_list = [
+    {'school_class': '4a', 'scores': [3,4,4,5,2]},
+    {'school_class': '5a', 'scores': [2,5,3,2,4]},
+    {'school_class': '6a', 'scores': [3,5,4,5,4]}
+]
+
+for i in score_list:
+    print(score_list['scores'])
+
+
---
+++ train.py
@@ -0,0 +1,2 @@
+score_list = {'school_class': '4a', 'scores': [3,4,4,5,2]}
+print(score_list['scores'])
